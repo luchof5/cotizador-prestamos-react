69ba67439dcca940e9dc961b61778a036744b224
Luis Furtado <lucho_l.f@hotmail.com>
2023-12-03 18:16:51 -0300

    plazo a pagar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Header from "./components/HEADER.JSX"
-import Button from './components/Button';
+import Button from './components/Button'
+import { fomratearDinero } from './helpers'
 
 function App() {
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
 
   const MIN = 0;
   const MAX = 20000;
@@ -61,9 +63,24 @@ function App() {
           value={cantidad}
         />
 
-        <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{cantidad}
-          
+        <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
+          {fomratearDinero(cantidad)}
         </p>
+
+        <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+          Elige un <span className='text-indigo-600'>Plazo </span>a pagar
+        </h2>
+
+        <select
+          className='mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500'
+          value={meses}
+          onChange={ e => setMeses(+e.target.value) }
+        >
+          <option value="6">6 Meses</option>
+          <option value="12">12 Meses</option>
+          <option value="24">24 Meses</option>
+        </select>
+
       </div>
   )
 }
